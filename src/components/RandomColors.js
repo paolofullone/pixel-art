121bf6dd3f59100f389866bfc88fb4ec754f1bd8
paolofullone <paolo.enrico@gmail.com>
2022-04-09 14:42:11 -0300

    Home basic css, component RandomColors created

---
+++ src/components/RandomColors.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RandomColors() {
+  return (
+    <div>RandomColors</div>
+  )
+}
+
+export default RandomColors
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import '../App.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="container">
+      <h1>Pixel Art 2.0</h1>
+    </div>
   )
 }
 
