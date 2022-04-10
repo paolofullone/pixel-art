55a434cd9020dee0c9ca939df722f298d36419c5
paolofullone <paolo.enrico@gmail.com>
2022-04-10 19:20:04 -0300

    feat color fill ok

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Context from '../context/Context';
+
 
 const Board = () => {
+    const {
+    color,
+    } = useContext(Context);
+  
     const boardSize = 20;
     const makeBoard = () => {
         let board = [];
@@ -9,11 +15,19 @@ const Board = () => {
             for (let j = 0; j < boardSize; j++) {
                 row.push(<div className="pixel-row" key={i + j}></div>)
             }
-            board.push(<div className="pixel" key={i}>{row}</div>)
+          board.push(
+            <div
+              className="pixel"
+              key={i}
+              onClick={addColor}
+            >{row}</div>)
         }
         return board;
     }
 
+  const addColor = (e) => {
+    e.target.style.backgroundColor = color;
+  }
 
   return (
     <div className="container margin-top">{makeBoard()}</div>
